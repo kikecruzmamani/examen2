b38b24aa51ca3730655e2358a4b42e84e068bbe8
kikecruzmamani <kikelike@gmail.com>
2016-11-12 10:52:18 -0400

    iniciando repo

---
+++ ejemplo1.js
@@ -0,0 +1,4 @@
+var pelicula={titulo:"xmen",genero:"accion",director:"marvel",duracion:50}
+console.log("pelicula: "+pelicula.titulo);
+console.log("pelicula: "+pelicula.genero);
+console.log("pelicula: "+pelicula.duracion);
